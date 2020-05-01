d0bb8c7f1605eb6d9542849b876602b0414ac1f7
Andrew Philpott <andrewphilpott92@gmail.com>
2020-05-01 10:21:15 -0700

    Add code to pass test.

--- src/reducers/beer-list-reducer.js
+++ src/reducers/beer-list-reducer.js
@@ -4,6 +4,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case a.NEW_BEER:
       return [...state, action.payload];
+    case a.DELETE_BEER:
+      return [...state].filter((x) => x.id !== action.payload);
   }
   return state;
 };
