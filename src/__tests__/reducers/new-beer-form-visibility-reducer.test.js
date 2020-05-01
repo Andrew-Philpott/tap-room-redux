4879430a770ee9d68f721ab9659bd0914b78adba
Andrew Philpott <andrewphilpott92@gmail.com>
2020-05-01 12:04:45 -0700

    Add code to pass test.

--- src/__tests__/reducers/new-beer-form-visibility-reducer.test.js
+++ src/__tests__/reducers/new-beer-form-visibility-reducer.test.js
@@ -5,4 +5,12 @@ describe("newBeerFormVisibilityReducer", () => {
   test("Should return default state if no action type is recognized", () => {
     expect(newBeerFormVisibilityReducer(false, { type: null })).toEqual(false);
   });
+
+  test("Should toggle the state between true and false", () => {
+    expect(
+      newBeerFormVisibilityReducer(false, {
+        type: c.TOGGLE_NEW_BEER_FORM_VISIBILITY,
+      })
+    ).toEqual(true);
+  });
 });
