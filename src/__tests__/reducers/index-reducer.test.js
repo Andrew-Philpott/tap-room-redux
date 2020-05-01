a03c9189dc3ca40161c9261dc336ad94fd9a463c
Andrew Philpott <andrewphilpott92@gmail.com>
2020-05-01 09:23:21 -0700

    Add test to check root reducer and beer list reducer initialize with the same state.

---
+++ src/__tests__/reducers/index-reducer.test.js
@@ -0,0 +1,15 @@
+import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import newBeerFormVisibilityReducer from "../../reducers/new-beer-form-visibility-reducer";
+import editBeerFormVisibilityReducer from "../../reducers/edit-beer-form-visibility-reducer";
+import beerListReducer from "../../reducers/beer-list-reducer.js";
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+  test("Check that initial state of beerListReducer matches root reducer", () => {
+    expect(store.getState().beerList).toEqual(
+      beerListReducer([], { type: null })
+    );
+  });
+});
--- src/__tests__/reducers/index.reducer.test.js
+++
@@ -1,4 +0,0 @@
-// import rootReducer from "../../reducers/index";
-// import { createStore } from "redux";
-// import editBeerFormVisibilityReducer from "../../reducers/edit-beer-form-visibility-reducer";
-// import newBeerFormVisibilityReducer from "../../reducers/new-beer-form-visibility-reducer";
